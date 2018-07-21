a49106f449c4c907348155ac1aa9d57f7773ac11
MD Servidores Cloud <34929556+servidorescloud@users.noreply.github.com>
2018-07-20 23:36:50 -0300

    Update payouts.sql

--- DB/payouts.sql
+++ DB/payouts.sql
@@ -1,26 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 4.7.7
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: 20-Jul-2018 às 20:09
--- Versão do servidor: 5.6.39
--- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `mdbrcom_nbr`
---
 
 -- --------------------------------------------------------
 
