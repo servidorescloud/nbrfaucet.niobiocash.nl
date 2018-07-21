57287f1cd4ba435c53ccbccb1b6c864388061378
MD Servidores Cloud <34929556+servidorescloud@users.noreply.github.com>
2018-07-20 23:31:19 -0300

    Add files via upload

---
+++ includes/conexao.php
@@ -0,0 +1,26 @@
+<?php
+ini_set('max_execution_time', 60);
+//set timezone
+date_default_timezone_set('America/Recife');
+
+//database credentials
+
+define('DBHOST2','localhost');
+define('DBUSER2','USUARIO-DB');
+define('DBPASS2','SENHA-DB');
+define('DBNAME2','NOME-DBr');
+
+try {
+
+    //create PDO connection 2
+    $db2 = new PDO("mysql:host=".DBHOST2.";dbname=".DBNAME2, DBUSER2, DBPASS2);
+    $db2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+    //show error
+    echo '<p class="bg-danger">'.$e->getMessage().'</p>';
+    exit;
+}
+
+
+?>
